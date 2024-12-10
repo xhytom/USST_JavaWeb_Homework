db1fa21ba63f53ccfd0426d36c1aaa8028068753
xhytom <jy34zhy@163.com>
2024-12-11 02:20:50 +0800

    vue-test2: 新增style绑定

---
+++ vue-test2/src/components/StyleDemo.vue
@@ -0,0 +1,31 @@
+<template>
+    <p :style="{color:activeColor, fontSize:fontSize + 'px'}">Style绑定1</p>
+    <p :style="styleObject">Style绑定2</p>
+    <p :style="[styleObject, styleObject2]">Style绑定3</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeColor: 'green',
+            fontSize: 40,
+            styleObject: {
+                color: 'red',
+                fontSize: '30px'
+            },
+
+            styleObject2: {
+                fontStyle: 'italic'
+            }
+        }
+    }
+}
+
+</script>
+
+<style>
+.text001 {
+    font-style: italic;
+}
+</style>
