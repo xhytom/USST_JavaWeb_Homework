152978ce3f5545c9dd288746307b810f333d67fd
xhytom <jy34zhy@163.com>
2024-12-11 02:35:09 +0800

    vue-test2: 新增侦听器

---
+++ vue-test2/src/components/WatchDemo.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3> 侦听器 </h3>
+    <p>{{ message }}</p>
+    <button @click="updateHandle">修改</button>
+
+    <Textarea>{{ inputMessage }}</Textarea>
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            message: "Hello",
+            inputMessage: "请输入"
+        }
+    },
+    methods: {
+        updateHandle() {
+            this.message = "World"
+        }
+    },
+    watch: {
+        message(newValue, oldValue) {
+            console.log(oldValue);
+            console.log(newValue);
+        },
+        inputMessage(newValue, oldValue) {
+            console.log(newValue, oldValue);
+            
+        }
+    }
+}
+
+</script>
+
