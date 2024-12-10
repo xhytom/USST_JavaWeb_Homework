b8bc2c4a1287fd16ddeafb5ecba7543a49d283eb
Caiguang <1004273473@qq.com>
2024-12-11 00:12:21 +0800

    添加前端学习代码

---
+++ vue-test2/src/components/IfDemo.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3> 条件渲染 </h3>
+    <div v-if="flag"> 你能看见我吗</div>
+    <div v-else> 你不能看见我</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            flag: false
+        }
+    }
+}
+
+</script>
---
+++ vue-test2/src/components/KeyDemo.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3> Key属性添加到v-for中</h3>
+    <p v-for="(item) in names" :key="index">{{ item }}</p>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            names: ["A", "B", "C"]
+        }
+    }
+}
+
+</script>
