6c2108d3e5ea214d66a6b6f386dbcae969dcbefb
xhytom <jy34zhy@163.com>
2024-12-12 18:55:41 +0800

    新增新闻网站后端

---
+++ news-website-backend/src/main/java/org/example/newswebsitebackend/NewsWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.newswebsitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsWebsiteBackendApplication.class, args);
+    }
+
+}
---
+++ news-website-backend/src/test/java/org/example/newswebsitebackend/NewsWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newswebsitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
