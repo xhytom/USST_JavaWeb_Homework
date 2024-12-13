4ba79c4bf87b2cb2eee0c7ce6f0e235b7bbeeeb3
Caiguang <1004273473@qq.com>
2024-12-13 14:33:42 +0800

    增加c3p0

---
+++ jdbctest/jdbctest/test005/src/com/test/Test.java
@@ -0,0 +1,50 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws PropertyVetoException, SQLException {
+        ComboPooledDataSource pool = new ComboPooledDataSource();
+        pool.setUser("root");
+        pool.setPassword("123456");
+        pool.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/test?serverTimezone=UTC");
+        pool.setDriverClass("com.mysql.jdbc.Driver");
+        pool.setMaxPoolSize(30);
+        pool.setAcquireRetryAttempts(3);
+        pool.setAcquireRetryDelay(1000);
+        Connection connection = pool.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id = 4");
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (!resultSet.next()) {
+            System.out.println("未找到");
+            return;
+        }
+        Long id = resultSet.getLong("id");
+        String phone = resultSet.getString("phone");
+        String password  = resultSet.getString("password");
+
+        System.out.println(id + phone + password);
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return;
+
+    }
+
+//    pool.setUser("root");
+}
