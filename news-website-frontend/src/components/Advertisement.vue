665fae49d2f5c87edd5713222f8d4e9b39104895
xhytom <jy34zhy@163.com>
2024-12-16 16:20:22 +0800

    新增广告获取api

--- news-website-frontend/src/components/Advertisement.vue
+++ news-website-frontend/src/components/Advertisement.vue
@@ -1,12 +1,39 @@
 <template>
     <div id="base">
-        <img id="pic" src="/public/images/advertisement.png" alt="image">
-        <p id="advertisementText">广告位招租</p>
+        <!-- <img id="pic" src="/public/images/advertisement.png" alt="image">
+        <p :id="advertisementText">广告位招租</p> -->
+        <div v-if="ad">
+            <h2 class="adTitle">{{ ad.title }}</h2>
+            <!-- 显示广告图片 -->
+            <img id="pic" :src="ad.imageUrl" alt="广告图片" />
+            <p class="adDesc">{{ ad.description }}</p>
+        </div>
+        <div v-else>
+            <p>加载广告...</p>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      ad: null,  // 存储广告数据
+    };
+  },
+  mounted() {
+    // 请求广告数据
+    axios.get('http://localhost:8080/api/advertisement/getAdvertisement')
+      .then((response) => {
+        this.ad = response.data;  // 设置广告数据
+      })
+      .catch((error) => {
+        console.error("加载广告失败", error);
+      });
+  },
+};
 </script>
 
 <style>
@@ -23,6 +50,17 @@
     justify-content: center;
 }
 #advertisementText {
+    text-align: center;
+    color: blue;
+    font-size:30px;
+}
+.adTitle {
+    text-align: center;
+    color: red;
+    font-size:30px;
+}
+
+.adDesc {
     text-align: center;
     color: blue;
     font-size:30px;
